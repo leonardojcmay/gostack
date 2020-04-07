e2815e031f4fe8412011ec8138a35868800314a9
Leonardo May <leonardojcmay@hotmail.com>
2020-04-07 14:31:21 -0300

    Nivel 01 - Front-end convertendo arquivo utilizando babel

---
+++ nivel-01/frontend/babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    presets: [
+        '@babel/preset-env',//vai converter funcionalidades que a aplicação não entende. Exemplo: converte o codigo para que funcione no internet explorer
+        '@babel/preset-react'//add as funcionalidades do react nesta conversão
+    ],
+};
